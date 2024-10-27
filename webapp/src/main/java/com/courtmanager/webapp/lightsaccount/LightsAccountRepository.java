20674cc2258077178c11e07dcf6441e71080b7d1
Lodewikes <greyvenstein.louw@gmail.com>
2024-10-27 09:19:45 +0200

    resolving jdbc dependencies

--- webapp/src/main/java/com/courtmanager/webapp/lightsaccount/LightsAccountRepository.java
+++ webapp/src/main/java/com/courtmanager/webapp/lightsaccount/LightsAccountRepository.java
@@ -1,1 +1,18 @@
+package com.courtmanager.webapp.lightsaccount;
 
+import com.courtmanager.webapp.databases.MsAccessDb;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class LightsAccountRepository {
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public LightsAccountRepository() {
+    this.jdbcTemplate = MsAccessDb.getInstance().getJdbcTemplate();
+  }
+
+  // Implement your CRUD operations as before
+}
